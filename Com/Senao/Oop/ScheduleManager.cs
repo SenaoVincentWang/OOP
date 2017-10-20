62d695060dc3c42223114aac1c8e22f1cbefa49e
Vincent.Wang <Vincent.Wang@senao.com.tw>
2017-10-20 20:25:35 +0800

    no message

---
+++ Com/Senao/Oop/ScheduleManager.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Com.Senao.Oop
+{
+    /// <summary>
+    /// 排程管理
+    /// </summary>
+    public class ScheduleManager
+    {
+        /// <summary>
+        /// 排程
+        /// </summary>
+        private List<Schedule> schedules;
+
+        public Schedule this[int index]
+        {
+            get
+            {
+                return schedules[index];
+            }
+        }
+
+        /// <summary>
+        /// 筆數
+        /// </summary>
+        /// <returns>排程筆數</returns>
+        public int Count()
+        {
+            return schedules.Count;
+        }
+
+        public void ProcessSchedules()
+        {
+            JObject scheduleData = JObject.Parse(File.ReadAllText("Schedule.json"));
+            JArray scheduleDataArray = (JArray)scheduleData["schedules"];
+            this.schedules = scheduleDataArray.ToObject<List<Schedule>>();
+        }
+    }
+}
